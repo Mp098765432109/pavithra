9094160e7fa6255e35d46535ffbc09103834f6e7
Mp098765432109 <144700836+Mp098765432109@users.noreply.github.com>
2024-03-05 21:55:24 -0800

    Add files via upload

---
+++ postdata.php
@@ -0,0 +1,26 @@
+<?php?>
+<head>
+<title>post</title>
+</head>
+<body>
+ <center>
+ <form action="post.php"method="post">
+    <div class="box1">
+    <label>1.english</label><input type="text"name="english"placeholder="enter your mark"value=" ">
+    </div>
+    <div class="box1">
+    <label>2.tamil<label><input type="text"name="tamil"placeholder="enter your mark"value=" ">
+    </div class="box1">
+    <label>3.maths</label><input type="text"name="maths"placeholder="enter your mark"value=" ">
+    </div>
+    <div class="box1">
+    <label>4.science</label><input type="text"name="science"placeholder="enter your mark"value=" ">
+    </div>
+    <div class="box1">
+    <label>5.socialscience</label><input type="text"name="socialscience"placeholder="enter your mark"value=" ">
+    </div>
+    <input class="button"type="submit"name="log"id="log"value="get avg">
+ </form>
+    </center>
+ </body>
+  
